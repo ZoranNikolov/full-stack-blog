b30440b7b9ab39fc811348bb258c32774540615d
ZoranNikolov <zoran902@gmail.com>
2024-02-28 10:23:56 +0200

    added post component

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,24 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
 
 function App() {
-	return <div className="App">
-		<Router>
-			<Link to="/createapost" > Create a Post </Link>
-			<Link to="/" > Home Page </Link>
-			<Routes>
-				<Route path="/" exact element={<Home/>}/>
-				<Route path="/createapost" exact element={<CreatePost/>}/>
-			</Routes>
-		</Router>
-	</div>;
+	return (
+		<div className="App">
+			<Router>
+				<div className="navbar">
+					<Link to="/createapost"> Create a Post </Link>
+					<Link to="/"> Home Page </Link>
+				</div>
+				<Routes>
+					<Route path="/" exact element={<Home />} />
+					<Route path="/createapost" exact element={<CreatePost />} />
+					<Route path="/post/:id" exact element={<Post />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
