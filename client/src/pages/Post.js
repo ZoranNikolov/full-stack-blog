237f65ff8365bc9f1c972bb35aa4c6df33364cf8
ZoranNikolov <zoran902@gmail.com>
2024-02-28 10:39:05 +0200

    added postId functionality

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Post() {
-    let {id} = useParams();
-  return (
-    <div>{id}</div>
-  )
+	let { id } = useParams();
+
+	useEffect(() => {
+		axios.get("http://localhost:3001/posts").then((response) => {});
+	});
+
+	return <div>{id}</div>;
 }
 
-export default Post
+export default Post;
