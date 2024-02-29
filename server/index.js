cd213e9c9e43d008dc0f7ea772420c66c8c76851
ZoranNikolov <zoran902@gmail.com>
2024-02-29 17:28:37 +0200

    updated

--- server/index.js
+++ server/index.js
@@ -8,12 +8,13 @@ app.use(cors());
 const db = require("./models");
 
 // Routers
-
-const postRouter = require('./routes/Posts');
-app.use('/posts', postRouter);
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
 
 db.sequelize.sync().then(() => {
-	app.listen(3001, () => {
-		console.log("server listening on port 3001");
-	});
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
